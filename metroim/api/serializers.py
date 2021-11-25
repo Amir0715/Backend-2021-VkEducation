0bd3a5e8e99054459e3bd79ba52bd91f6b040215
amir kamolov <kamolov.amir2000@yandex.ru>
2021-11-25 17:10:39 +0300

    [+] validations

--- metroim/api/serializers.py
+++ metroim/api/serializers.py
@@ -1,18 +1,41 @@
 from rest_framework import serializers
 from ui.models import City, Line, Station
 
+
 class StationSerializer(serializers.ModelSerializer):
+
+    def validate(self, attrs):
+        if attrs['latitude'] < -180 or attrs['latitude'] > 180:
+            raise serializers.ValidationError(
+                "latitude must be in range [-180, 180]")
+        if attrs['longitude'] < -90 or attrs['longitude'] > 90:
+            raise serializers.ValidationError(
+                "longitude must be in range [-90, 90]")
+        return attrs
 
     class Meta:
         model = Station
         fields = ('id', 'name', 'order', 'latitude', 'longitude', 'line')
 
+
 class LineSerializer(serializers.ModelSerializer):
     stations = StationSerializer(many=True, read_only=True)
+
+    def validate(self, attrs):
+        if len(attrs['hex_color']) != 6:
+            raise serializers.ValidationError(
+                "the length of hex_color should be equal to 6")
+        try:
+            int(attrs['hex_color'], 16)
+        except ValueError:
+            raise serializers.ValidationError(
+                "hex_color must be hexadecimal color")
+        return attrs
 
     class Meta:
         model = Line
         fields = ('id', 'name', 'hex_color', 'city', 'stations')
+
 
 class CitySerializer(serializers.ModelSerializer):
     lines = LineSerializer(many=True, read_only=True)
@@ -20,4 +43,3 @@ class CitySerializer(serializers.ModelSerializer):
     class Meta:
         model = City
         fields = ('id', 'name', 'lines')
-
--- metroim/ui/models.py
+++ metroim/ui/models.py
@@ -2,19 +2,23 @@ from django.db import models
 from django.db.models.fields.related import ForeignKey, ManyToManyField
 from django.template.defaultfilters import slugify
 
+
 class City(models.Model):
     name = models.CharField('Название города', max_length=64)
+
 
 class Line(models.Model):
     name = models.CharField('Название линии', max_length=64)
     # В будущем создать кастомное поле для работы с hex-числом
     hex_color = models.CharField('Название линии', max_length=6)
-    city = ForeignKey('City', verbose_name='Город', on_delete=models.CASCADE, related_name='lines')
-    
+    city = ForeignKey('City', verbose_name='Город',
+                      on_delete=models.CASCADE, related_name='lines')
+
+
 class Station(models.Model):
     name = models.CharField('Название станции', max_length=64)
     order = models.PositiveSmallIntegerField('Порядок')
     latitude = models.FloatField('Долгота')
     longitude = models.FloatField('Широта')
-    line = ForeignKey('Line', verbose_name='Линии', on_delete=models.CASCADE, related_name='stations')
-    
+    line = ForeignKey('Line', verbose_name='Линии',
+                      on_delete=models.CASCADE, related_name='stations')
