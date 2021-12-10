6314a2cfd4b8931f45456cc4434877bb86036130
amir kamolov <kamolov.amir2000@yandex.ru>
2021-12-10 05:09:52 +0300

    [+] done hw9

--- hw9/threading/client.py
+++ hw9/threading/client.py
@@ -1,31 +1,45 @@
 from queue import Queue
 import socket
 import threading
+import argparse
+import logging
 
 
 class Client:
     urls = Queue()
+    logger: logging.Logger = logging.getLogger("client")
 
     def __init__(self, address, filepath, thread_count) -> None:
+        self.logger.setLevel(logging.DEBUG)
+        fh = logging.FileHandler('client.log', mode='w')
+        fh.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+        self.logger.addHandler(fh)
+
         self.address = address
         self.read_file(filepath)
+
+        self.logger.debug(f'[{threading.current_thread().getName()}] Client instance has been created with [{address=}] [{filepath=}] [{thread_count=}]')
+
         self.threads = [
             threading.Thread(target=self.worker, args=(self.urls, ), daemon=True)
             for _ in range(thread_count)
         ]
+        self.logger.debug(f'[{threading.current_thread().getName()}] {thread_count} threads has been created')
 
     def run(self):
         for th in self.threads:
             th.start()
+            self.logger.debug(f'[{threading.current_thread().getName()}] {th.getName()} starting...')
         for th in self.threads:
             th.join()
+            self.logger.debug(f'[{threading.current_thread().getName()}] {th.getName()} joining...')
 
     def read_file(self, filepath):
         with open(filepath) as f:
             urls = f.read().splitlines()
             for url in urls:
                 self.urls.put(url)
-            print(f"{len(urls)} urls have been added")
+            self.logger.debug(f"[{threading.current_thread().getName()}] {len(urls)} urls have been added")
 
     def worker(self, urls: Queue[str]):
         while True:
@@ -33,21 +47,31 @@ class Client:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
             sock.connect(self.address)
-            print(f"[{threading.current_thread().name}]: connect to {self.address}!")
+            self.logger.debug(f"[{threading.current_thread().name}]: connect to {self.address}!")
 
             sock.sendall(bytes(url+"\n", 'utf-8'))
-            print(f"[{threading.current_thread().name}]: {url} have been sended!")
+            self.logger.debug(f"[{threading.current_thread().name}]: {url} have been sended!")
 
             rcv = str(sock.recv(1024), "utf-8")
-            print(f"[{threading.current_thread().name}]: waiting responce")
+            self.logger.debug(f"[{threading.current_thread().name}]: waiting responce")
 
             if rcv:
-                print(f"[{threading.current_thread().name}]: {url} : {rcv}")
-
+                self.logger.debug(f"[{threading.current_thread().name}]: {url} : {rcv}")
+                print(f"{url}: {rcv}")
             sock.close()
 
 
+def createParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file', type=str, default='urls')
+    parser.add_argument('-t', '--threads', type=int, default=1)
+    parser.add_argument('-p', '--port', type=int, default=8000)
+    return parser
+
+
 if __name__ == "__main__":
-    ADDRESS = "localhost", 8000
-    client = Client(ADDRESS, 'urls', 7)
+    namespace = createParser().parse_args()
+
+    ADDRESS = "localhost", namespace.port
+    client = Client(ADDRESS, namespace.file, namespace.threads)
     client.run()
--- hw9/threading/server.py
+++ hw9/threading/server.py
@@ -1,8 +1,10 @@
+import argparse
 import socketserver
 import threading
 import logging
-from worker import Worker
+from typing import Counter
 import requests
+import json
 
 
 class ThreadedRequestHandler(socketserver.StreamRequestHandler):
@@ -12,33 +14,40 @@ class ThreadedRequestHandler(socketserver.StreamRequestHandler):
         self.server.logger.debug(f"[{threading.current_thread().getName()}]: {self.client_address} send {data}")
         json = self.worker(data)
         # отправляем данные
-        self.wfile.write(bytes(str(json.status_code)+'\n', 'UTF-8'))
+        self.wfile.write(bytes(str(json), 'UTF-8'))
 
     def worker(self, url):
         self.server.logger.debug(f"[{threading.current_thread().getName()}]: thread waiting!")
         with self.server.sem:
             self.server.logger.debug(f"[{threading.current_thread().getName()}]: thread join!")
             res = self.top_k(url)
+            d = {}
+            for k, v in res:
+                d[k] = v
+            d = json.dumps(d)
             self.server.logger.debug(f"[{threading.current_thread().getName()}]: thread left!")
-            return res
+            return d
 
     def top_k(self, url):
         data = requests.get(url)
         self.server.logger.debug(f"[{threading.current_thread().getName()}]: [{url}]: {data}")
-        return data
+        return Counter(data.text).most_common(self.server.k)
 
 
 class Master(socketserver.ThreadingTCPServer):
     sem = None
     logger: logging.Logger = logging.getLogger("server")
     allow_reuse_address = True
+    k = 0
 
-    def __init__(self, host: str, port: int, handler_classRequestHandler: socketserver.BaseRequestHandler, worker_limit: int):
+    def __init__(self, host: str, port: int, handler_classRequestHandler: socketserver.BaseRequestHandler, worker_limit: int, k: int):
         super().__init__((host, port), handler_classRequestHandler)
 
         self.sem = threading.Semaphore(worker_limit)
+        self.k = k
+
         self.logger.setLevel(logging.DEBUG)
-        fh = logging.FileHandler('server.log')
+        fh = logging.FileHandler('server.log', mode='w')
         fh.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
         self.logger.addHandler(fh)
         debug_str = f'Create server instanse with host {host}, port {port}'
@@ -55,9 +64,16 @@ def run():
         m.serve_forever()
 
 
+def createParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-w', '--workers', type=int, default=1)
+    parser.add_argument('-k', '--top_k', type=int, default=7)
+    parser.add_argument('-p', '--port', type=int, default=8000)
+    return parser
+
+
 if __name__ == '__main__':
-    address = ('localhost', 8000)
-    t = threading.Thread(target=run)
-    t.setDaemon(True)
-    t.start()
-    t.join()
+    namespace = createParser().parse_args()
+    address = ('localhost', namespace.port)
+    with Master(*address, ThreadedRequestHandler, namespace.workers, namespace.top_k) as m:
+        m.serve_forever()
