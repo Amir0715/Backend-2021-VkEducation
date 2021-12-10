88c0a9b6142404af65f6d2bdffc8fdefedfb0fec
amir kamolov <kamolov.amir2000@yandex.ru>
2021-12-10 03:37:13 +0300

    [+] Добавил сервер и клиент

--- hw9/threading/client.py
+++ hw9/threading/client.py
@@ -1,29 +1,53 @@
 from queue import Queue
 import socket
-import sys
-import time
+import threading
 
-HOST, PORT = "localhost", 8000
-q = Queue()
 
-def fetch():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((HOST, PORT))
-    data = input("Client: ")
-    if data == "q":
-        exit()
-    else:
-        sock.sendall(bytes(data+"\n", 'utf-8'))
-        rcv = str(sock.recv(1024), "utf-8")[1:]
-        if rcv:
-            q.put(rcv)
-    sock.close()
+class Client:
+    urls = Queue()
 
-# Create a socket (SOCK_STREAM means a TCP socket)
+    def __init__(self, address, filepath, thread_count) -> None:
+        self.address = address
+        self.read_file(filepath)
+        self.threads = [
+            threading.Thread(target=self.worker, args=(self.urls, ), daemon=True)
+            for _ in range(thread_count)
+        ]
+
+    def run(self):
+        for th in self.threads:
+            th.start()
+        for th in self.threads:
+            th.join()
+
+    def read_file(self, filepath):
+        with open(filepath) as f:
+            urls = f.read().splitlines()
+            for url in urls:
+                self.urls.put(url)
+            print(f"{len(urls)} urls have been added")
+
+    def worker(self, urls: Queue[str]):
+        while True:
+            url = urls.get()
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+            sock.connect(self.address)
+            print(f"[{threading.current_thread().name}]: connect to {self.address}!")
+
+            sock.sendall(bytes(url+"\n", 'utf-8'))
+            print(f"[{threading.current_thread().name}]: {url} have been sended!")
+
+            rcv = str(sock.recv(1024), "utf-8")
+            print(f"[{threading.current_thread().name}]: waiting responce")
+
+            if rcv:
+                print(f"[{threading.current_thread().name}]: {url} : {rcv}")
+
+            sock.close()
+
+
 if __name__ == "__main__":
-    fetch()
-    while True:
-        if q.not_empty:
-            print(q.get())
-        else: 
-            break
+    ADDRESS = "localhost", 8000
+    client = Client(ADDRESS, 'urls', 7)
+    client.run()
--- hw9/threading/server.py
+++ hw9/threading/server.py
@@ -1,42 +1,42 @@
 import socketserver
 import threading
-import typing
-import queue
 import logging
-
 from worker import Worker
+import requests
 
 
 class ThreadedRequestHandler(socketserver.StreamRequestHandler):
-    # logger: logging.Logger = logging.getLogger("StreamRequestHandler")
-
-    # def setup(self) -> None:
-    #     self.logger.setLevel(logging.DEBUG)
-    #     fh = logging.FileHandler('requests.log')
-    #     fh.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-    #     self.logger.addHandler(fh)
-    #     # self.logger.debug(f'Request setup')
-    #     return super().setup()
 
     def handle(self):
-        self.data = self.rfile.readline().strip()
-        self.server.urls.put(self.data)
-        print(f"{self.data}, len(urls) = {self.server.urls.qsize()}")
-        self.wfile.write(self.data)
+        data = self.rfile.readline().strip()  # получаем данные
+        self.server.logger.debug(f"[{threading.current_thread().getName()}]: {self.client_address} send {data}")
+        json = self.worker(data)
+        # отправляем данные
+        self.wfile.write(bytes(str(json.status_code)+'\n', 'UTF-8'))
 
-# TODO: Отнаследоваться от socketserver.ThreadingTCPServer,
-# что бы можно было получить доступ к полям мастера через объект 
-# self.server в RequestHandler классе
+    def worker(self, url):
+        self.server.logger.debug(f"[{threading.current_thread().getName()}]: thread waiting!")
+        with self.server.sem:
+            self.server.logger.debug(f"[{threading.current_thread().getName()}]: thread join!")
+            res = self.top_k(url)
+            self.server.logger.debug(f"[{threading.current_thread().getName()}]: thread left!")
+            return res
+
+    def top_k(self, url):
+        data = requests.get(url)
+        self.server.logger.debug(f"[{threading.current_thread().getName()}]: [{url}]: {data}")
+        return data
 
 
 class Master(socketserver.ThreadingTCPServer):
-    urls: queue.Queue[str] = queue.Queue()
-    workers: list[threading.Thread] = []
+    sem = None
     logger: logging.Logger = logging.getLogger("server")
+    allow_reuse_address = True
 
-    def __init__(self, host: str, port: int, handler_classRequestHandler: socketserver.BaseRequestHandler, worker_count: int):
+    def __init__(self, host: str, port: int, handler_classRequestHandler: socketserver.BaseRequestHandler, worker_limit: int):
         super().__init__((host, port), handler_classRequestHandler)
 
+        self.sem = threading.Semaphore(worker_limit)
         self.logger.setLevel(logging.DEBUG)
         fh = logging.FileHandler('server.log')
         fh.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
@@ -45,20 +45,14 @@ class Master(socketserver.ThreadingTCPServer):
         self.logger.debug(debug_str)
 
         print(debug_str)
-        self._create_workers(worker_count)
 
     def _get_statistic(self):
         pass
 
-    def _create_workers(self, worker_count: int):
-        for i in range(worker_count):
-            worker = Worker(self.urls)
-            # thread = threading.Thread(Worker.run)
 
 def run():
     with Master(*address, ThreadedRequestHandler, 7) as m:
         m.serve_forever()
-    
 
 
 if __name__ == '__main__':
